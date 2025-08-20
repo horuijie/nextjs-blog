dfddc689596851260bbfa72c6ce22a3940c1b7df
horuijie <horuijie87@gmail.com>
2025-08-20 16:08:13 +0800

    migrate to tailwind v4 import style

--- tailwindcss.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    content: [
-        './pages/**/*.{js,ts,jax,tsx}',
-        './components/**/*.{js,ts,jax,tsx}',
-    ],
-};
-
-//This one is for the best performance to avoid from false positives
-//need to be specific as possible about the content config
